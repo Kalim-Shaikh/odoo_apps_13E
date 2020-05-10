b294bd1290047f3c207dbb69a975606af7a1767f
MKShaikh <Ent-kalim>
2020-05-10 14:22:16 +0530

    Helpdesk Ticket Status and Subject Validation

---
+++ helpdesk_ticket_form_subject_validation/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Helpdesk Ticket Form Subject Validation',
+    'version' : '1.0',
+    'summary': 'Helpdesk Ticket Form Subject Validation',
+    'sequence': 15,
+    'description': """
+Helpdesk Ticket Form Subject Validation
+Helpdesk Ticket Form Subject Validation
+====================
+.
+    """,
+    'category': 'Operations/Helpdesk',
+    'images' : [],
+    'depends' : [
+        'website_helpdesk_form',
+    ],
+    'data': [
+        'views/helpdesk_template.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
